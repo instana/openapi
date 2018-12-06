74d40d60271f2625dfdb503efb3f7ed67e0c68c2
Bonsch <markus.bonsch@instana.com>
2018-12-06 13:47:19 +0100

    initial commit

---
+++ scripts/build.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+'use strict';
+
+require('shelljs/global');
+set('-e');
+
+mkdir('-p', 'web_deploy')
+
+cp('-R', 'web/*', 'web_deploy/');
+
+exec('npm run swagger bundle --        -o web_deploy/swagger.json');
+exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
+
+
