0706051bb77236a947bf11c0f8176cb59038f392
Bonsch <markus.bonsch@instana.com>
2019-05-14 13:26:20 +0200

    payload example post process

--- spec/plugins/x-payload.js
+++ spec/plugins/x-payload.js
@@ -1,29 +1,38 @@
 var path = require('path');
 var fs = require('fs');
+var jsyaml = require('js-yaml');
 var basedir = 'spec/';
 
 module.exports = {
-  pathExpression: '$..["x-description"]',
+  pathExpression: '$..["x-payload"]',
   init: function (_swagger, options) {
     if (options.verbose) {
-      console.log('* x-description plugin: ');
+      console.log('* x-payload plugin: ');
     }
   },
   process: function (parent, name, jsonpath, swagger) {
     var value = parent[name];
-    var descDir = basedir + 'descriptions/';
+    var descDir = basedir + 'payload/';
     var res = null;
     if (dirExist(descDir)) {
-      var fileName = descDir + value + '.md';
+      var fileName = descDir + value + '.json';
       if (fileExist(fileName)) {
-        delete parent['description'];
-        var description = fs.readFileSync(fileName, 'utf-8');
-        if (!isStringEmpty(description)) {
-          parent.description = description;
+        delete parent['x-payload'];
+        var payload = fs.readFileSync(fileName, 'utf-8');
+        if (!isStringEmpty(payload)) {
+          if (!parent.content) {
+            parent.content = {};
+          }
+          if (!parent.content['application/json']) {
+            parent.content['application/json'] = {};
+          }
+          parent.content['application/json'].examples = {}
+          parent.content['application/json'].examples.payload = {}
+          parent.content['application/json'].examples.payload.value = jsyaml.safeLoad(payload);
         }
       } else {
         try {
-          fs.writeFileSync(fileName,'');
+          fs.writeFileSync(fileName, '');
           console.error("file not exist and created, please commit to git: " + fileName);
         } catch (e) {
           console.log("Cannot write file " + fileName, e);
